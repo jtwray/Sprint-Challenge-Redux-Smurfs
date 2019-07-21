40d6514d31ef47163f33c8fc2f0786844d42a4e4
jtwray <tuckerwray84@gmail.com>
2019-07-21 19:16:47 -0400

    creates initialState const and rootReducer with switch cases for loading/success/error fininteStates of actions:fetchsmurfs&add smurf

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,64 +1,66 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
-import { LOADING, SUCCESS, ERROR} from "../actions";
-const initialState = {
-  smurfs: [],  
-  fetching: false,
-  error: null,
-  addingSmurf:false
-  // Array charac ters, Boolean fetching, null error.
-};
-export const charsReducer = (state = initialState, action) => {
-  switch( action.type ) {
-    case LOADING: {
-      // action types should be FETCHING, SUCCESS and FAILURE	   
-      // your switch statement should handle all of these cases.	   
-      console.log( action.payload )
-      return {
-          ...state,
-          fetching: true,
-        }
-      }
-      case SUCCESS: {
-        return {
-          ...state,
-          fetching: false,
-          error: null,
-          characters: action.payload.results
-        }
-      }
-      case ERROR: {
-        return {
-          ...state,
-          fetching: false,
-          error: action.payload
-        }
-      }
-    // Fill me in with the important reducers
-    // action types should be FETCHING, SUCCESS and FAILURE
-    // your switch statement should handle all of these cases.
-    default:
-      return state;
-  }
-};
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
+  /**Be sure to import in all of the action types from `../actions`
+  */
+  import {
+    LOADING_FETCHSMURFS,
+    LOADING_ADDSMURF,
+    SUCCESS_ADDSMURF,
+    SUCCESS_FETCHSMURFS,
+    ERROR,
+  } from "../actions/";
+  
+  /*
+   Your initial/default state for this project could *Although does not have to* look a lot like this
    updatingSmurf: false
    deletingSmurf: false
-   error: null
- }
-*/
-
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+  
+  */const initialState={
+    smurfs: [],
+    fetchSmurfs: false,
+    addSmurf: false,r
+    error: null,
+  }
+  /*
+    You'll only need one smuf reducer for this project.
+    Feel free to export it as a default and import as rootReducer. 
+    This will guard your namespacing issues.
+    There is no need for 'combineReducers' in this project.
+    Components can then read your store as, `state` and not `state.fooReducer`.
+  */
+  const rootReducer=(state=initialState,action)=>{
+    switch(action.type){
+      case LOADING_FETCHSMURFS:
+        return{
+          ...state,
+          fetchSmurfs:true
+  
+        };
+        case SUCCESS_FETCHSMURFS:
+          return{
+            ...state,
+            smurfs:[...action.payload],
+            fetchSmurfs:false
+  
+          };
+          case ERROR:
+            return{
+              ...state,
+              error:action.payload,
+              };
+              case: LOADING_ADDSMURF:
+              return{
+                ...state,
+                addSmurf:true,
+  
+              };
+              case SUCCESS_ADDSMURF:
+                return{
+                  ..state,
+                  smurfs:[...action.payload],
+                  addSmurf:false
+  
+                };
+                default:
+                  return state;
+      }};
+  export default rootReducer;
